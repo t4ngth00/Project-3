dd8a913e70c50849964683d4becdaa0d725d7ca1
t4ngth00 <t4ngth00@students.oamk.fi>
2016-11-18 03:54:30 +0200

    initial commit

---
+++ client/js/script.js
@@ -0,0 +1,31 @@
+$(function(){
+
+  var messages = [];
+  var peer_id, name, conn;
+
+  var peer = new Peer({
+    host: 'localhost',
+    port: 9000,
+    path: '/peerjs',
+    debug: 3,
+    config: {'iceServers': [
+    { url: 'stun:stun1.l.google.com:19302' },
+    { url: 'turn:numb.viagenie.ca',
+      credential: 'muazkh', username: 'webrtc@live.com' }
+    ]}
+  });
+
+  peer.on('open', function(){
+    $('#id').text(peer.id);
+  });
+
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  function getVideo(callback){
+    navigator.getUserMedia({audio: true, video: true}, callback, function(error){
+      console.log(error);
+      alert('An error occured. Please try again');
+    });
+  }
+
+});
